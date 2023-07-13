48f89f2083b0004cb0c435f17f8e01c5e102c3b1
FranckDodo91 <devlogappdgmf@gmail.com>
2023-07-13 09:54:38 +0200

    Added the MongoDB DB connection link in the "dbConnect.js" file

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dbConnect = async () => {
     try {
-        await mongoose.connect('url');
+        await mongoose.connect('mongodb+srv://dodoglazmichelfranck:3mdruTuch7D7ub@nodejsschoolmanagementm.ciop2ro.mongodb.net/NodejsSchoolManagementMystemApi?retryWrites=true&w=majority');
         console.log("DB connected successfully");   
     } catch (error) {
         console.log("DB connection failed : ", error.message);  
