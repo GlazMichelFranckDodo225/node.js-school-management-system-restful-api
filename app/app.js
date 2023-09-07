d78e5be64e94079470105946e0ad5cb9442c8b3e
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-09-07 06:03:00 +0200

    Custom Global Error Handler

--- app/app.js
+++ app/app.js
@@ -11,5 +11,23 @@ app.use(express.json());
 // === Routes ==
 app.use(adminBaseUrl, adminRouter);
 
+// Custom Global Error Handler
+app.use((err, req, res, next) => {
+    // console.log(err);
+    // Stack
+    const stack = err.stack;
+    // Actual message
+    const message = err.message;
+    // Status of message
+    const status = err.status ? err.status : "Failed";
+    const statusCode = err.statusCode ? err.statusCode : 500;
+
+    res.status(statusCode).json({
+        status,
+        message,
+        stack
+    });
+});
+
 module.exports = app;
 
